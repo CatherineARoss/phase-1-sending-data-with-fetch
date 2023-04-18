b4055d3116184c4b2d69b77c9ae46531f39e6ca0
Catherine Ross <fibervibes@gmail.com>
2023-04-18 11:39:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = { name: name, email: email };
+  
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        const idDiv = document.createElement('div');
+        idDiv.innerHTML = id;
+        document.body.appendChild(idDiv);
+      })
+      .catch(error => {
+        const errorDiv = document.createElement('div');
+        errorDiv.innerHTML = error.message;
+        document.body.appendChild(errorDiv);
+      });
+  }
+
+
+
+
+
+
